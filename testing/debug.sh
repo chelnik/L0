d3da125a8d8d5db8102606efd72443541e8f0fd3
Corki Udyr <corkiudy@student.21-school.ru>
2022-09-07 16:18:25 +0300

    add parse directory

--- testing/debug.sh
+++
@@ -1,5 +0,0 @@
-curl http://127.0.0.1:8080/debug/pprof/heap -o mem_out.txt
-curl http://127.0.0.1:8080/debug/pprof/profile?seconds=5 -o cpu_out.txt
-# Далее, используя саму команду pprof, мы можем построить .svg файл по полученным результатам.
-go tool pprof -svg -alloc_objects pprof_1.sh mem_out.txt > mem_ao.svg
-go tool pprof -svg pprof_1.sh cpu_out.txt > cpu.svg
